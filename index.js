c1e6b5ffc39203733ed3ce2678227b88dc1c839f
aayahda <aadhyakraj@gmail.com>
2024-02-11 14:38:06 +0530

    initial code

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db= new pg.Client({
+  user:"postgres",
+  host:"localhost",
+  database:"world",
+  password:"AadhyaRaj@0!",
+  port:"5432"
+})
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  const result =await db.query("select country_code from visited_countries");
+  let countries=[];
+  
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
