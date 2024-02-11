5f1b09c6726f5eb423b15d221c5e194e8ceb3a93
aayahda <aadhyakraj@gmail.com>
2024-02-11 20:01:14 +0530

    countries added

--- index.js
+++ index.js
@@ -20,14 +20,27 @@ app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   const result =await db.query("select country_code from visited_countries");
+  // console.log(result);
   let countries=[];
   result.rows.forEach((country)=> {
     countries.push(country.country_code);
   });
-  console.log(result.rows);
-  console.log(countries);
+  
+  // console.log(result.rows);
+  // console.log(countries);
   res.render("index.ejs",{countries:countries,total:countries.length});
-db.end();
+
+});
+
+app.post("/add",async(req,res)=>{
+  const user_country =req.body["country"];
+  const result = await db.query("Select country_code from countries where country_name = $1 ",[user_country]);
+  // console.log(result);
+  // console.log(result.rows[0].country_code);
+  if (result.rows.length!==0){
+    await db.query("Insert into visited_countries (country_code) values ($1)",[result.rows[0].country_code]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
